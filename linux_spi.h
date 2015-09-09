b972201a9629cc061ebe2ce543170b7ba37123bf
KoynovStas <skojnov@yandex.ru>
2015-09-09 11:39:50 +0300

    add enum SPI_Error

--- linux_spi.h
+++ linux_spi.h
@@ -11,6 +11,23 @@ class Linux_SPI
     public:
 
 
+        enum SPI_Error{
+
+            ERROR_DEV_NOT_OPEN,
+            ERROR_CANT_OPEN_DEV,
+            ERROR_CANT_SET_MODE,
+            ERROR_CANT_GET_MODE,
+            ERROR_CANT_SET_BITS,
+            ERROR_CANT_GET_BITS,
+            ERROR_CANT_SET_SPEED,
+            ERROR_CANT_GET_SPEED,
+            ERROR_BAD_PARAM,
+            ERROR_CANT_READ,
+            ERROR_CANT_WRITE,
+            ERROR_CANT_SEND_TR
+        };
+
+
          Linux_SPI();
         ~Linux_SPI();
 
